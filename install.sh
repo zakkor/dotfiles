576134519aa1946aa08d603f6c7a1526c86e430f
zakkor <edward.partenie@gmail.com>
2020-01-19 01:09:37 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Destination dir overrides:
+# The dir name's value in this map will be used as the stow --target arg
+# Example: ["nix"]="/etc/nixos/" => stow --target "/etc/nixos/" nix
+declare -A destinations
+destinations=(
+  ["nixos"]="/etc/nixos/"
+)
+
+# Check if stow is installed
+stow --version > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "stow is not installed"
+  exit 1
+fi
+
+for dir in */ ; do
+  dest="${destinations[${dir%/}]}"
+  if [ -z "$dest" ]
+  then
+    stow -v "$dir"
+  else
+    stow -v -t "$dest" "$dir"
+  fi
+done
