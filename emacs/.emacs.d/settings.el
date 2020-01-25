01c36b70e3c7f82d59dfb4953570eaf6cd07fc4b
zakkor <edward.partenie@gmail.com>
2020-01-25 12:15:56 +0200

    emacs: nlinum

--- emacs/.emacs.d/settings.el
+++ emacs/.emacs.d/settings.el
@@ -1,5 +1,10 @@
 ;; Save sessions
-;(desktop-save-mode 1)
+(desktop-save-mode 1)
+;; The default desktop is saved always
+(setq desktop-save t)
+;; The default desktop is loaded anyway if it is locked
+(setq desktop-load-locked-desktop t)
+
 
 ;; Hide tool bar
 (tool-bar-mode -1)
@@ -17,3 +22,9 @@
   :weight 'normal
   :width 'normal)
 (set-frame-font "Roboto Mono" nil t)
+
+(setq inhibit-startup-screen t)
+
+(use-package nlinum
+  :config
+  (setq linum-format " %d "))
