6f936b40fb731abbc8370cd5caa488005c096515
zakkor <edward.partenie@gmail.com>
2020-01-25 12:15:22 +0200

    emacs: no doom theme for treemacs as it breaks fonts

--- emacs/.emacs.d/theme.el
+++ emacs/.emacs.d/theme.el
@@ -4,16 +4,10 @@
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-one t)
-  
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  ;(doom-themes-neotree-config)
-  ;; or for treemacs users
-  ;(setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
-  ;(doom-themes-treemacs-config)
-  
-  ;; Corrects (and improves) org-mode's native fontification.
-  ;(doom-themes-org-config)
-  )
+  ;; Disable modeline in treemacs
+  (add-hook 'treemacs-mode-hook
+	    (lambda ()
+	      (setq mode-line-format nil))))
 
 (use-package doom-modeline
     :hook (after-init . doom-modeline-mode)
@@ -24,11 +18,5 @@
     (defun setup-custom-doom-modeline ()
         (doom-modeline-set-modeline 'default-line 'default))
     (add-hook 'doom-modeline-mode-hook 'setup-custom-doom-modeline)
-
     (setq doom-modeline-icon nil)
-    (setq doom-modeline-buffer-state-icon nil)
-    ; (setq doom-modeline-minor-modes t)
-    ;; Height
-    (setq doom-modeline-height 1)
-    (set-face-attribute 'mode-line nil :height 100)
-    (set-face-attribute 'mode-line-inactive nil :height 100))
+    (setq doom-modeline-buffer-state-icon nil))
