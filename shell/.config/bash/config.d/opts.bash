158725120a81095eb85ec048eb79688297102b9a
Zakkor <edward.partenie@gmail.com>
2020-02-20 12:09:53 +0200

    bash: unlimited history

--- shell/.config/bash/config.d/opts.bash
+++ shell/.config/bash/config.d/opts.bash
@@ -30,3 +30,15 @@ bind "set show-all-if-ambiguous on"
 # Perform partial completion on the first Tab press,
 # only start cycling full results on the second Tab press
 bind "set menu-complete-display-prefix on"
+
+# Eternal bash history.
+# Undocumented feature which sets the size to "unlimited".
+# http://stackoverflow.com/questions/9457233/unlimited-bash-history
+export HISTFILESIZE=
+export HISTSIZE=
+# Change the file location because certain bash sessions truncate .bash_history file upon close.
+# http://superuser.com/questions/575479/bash-history-truncated-to-500-lines-on-each-login
+export HISTFILE=~/.bash_eternal_history
+# Force prompt to write history after every command.
+# http://superuser.com/questions/20900/bash-history-loss
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
