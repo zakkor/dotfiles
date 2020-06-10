0b67898dadb123b42440e54015c95b2e0ab9bde0
zakkor <edward.partenie@gmail.com>
2020-06-10 14:38:32 +0300

    acme: fix utils

--- acme/.config/bash/config.d/acme.bash
+++ acme/.config/bash/config.d/acme.bash
@@ -1,4 +1,13 @@
+#PLAN9="$(dirname $(dirname $(readlink $(which 9))))/plan9"
+PLAN9=/nix/store/95anarn3qmi373x0s60r9j0zwzn7dv7f-plan9port-2019-02-25/plan9
+
 if [ $TERM = "dumb" ]; then
+
+if [ "$(pgrep plumber)" ]; then
+	9 plumber
+fi
+
+cat "$PLAN9/plumb/basic" ~/.acme/plumbing | 9 write plumb/rules
 
 export PS1="$PS1\n"
 unalias ls
