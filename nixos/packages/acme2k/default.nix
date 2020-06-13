83a20feeba0d34d0387a46f634b029db3390d7ff
Zakkor <edward.partenie@gmail.com>
2020-06-13 19:27:57 +0300

    acme: add indent with spaces option from https://github.com/mkhl/plan9port/commit/c805dd5b05da6eec93b951c40c6792ed14343b6b, clean up config

--- nixos/packages/acme2k/default.nix
+++ nixos/packages/acme2k/default.nix
@@ -1,5 +1,5 @@
 {
-	pkgs ? import <nixpkgs> {}
+  pkgs ? import <nixpkgs> {}
   , runCommand
   , fetchFromGitHub
 }:
@@ -26,43 +26,103 @@ pkgs.plan9port.overrideAttrs(oldAttrs: {
     '';
     
     patches = [
+      ./indentSpaces.patch
       ./controlS-Put.patch
     ];
 
     postPatch = ''
-    	# disable bartflag
-    	substituteInPlace src/cmd/acme/config.h \
-    		--replace "int bartflag			= TRUE;" "int bartflag			= FALSE;"
-    
-      # hardcoded path
-      substituteInPlace src/cmd/acme/config.h \
-        --replace /lib/font/bit/profont/profont-12.font /mnt/font/RobotoMono-Regular/12a/font \
-        --replace /lib/font/bit/mntcarlo/mntcarlo.font /mnt/font/RobotoMono-Regular/12a/font
-      
-      # color scheme
-      echo -e "#define ENABLE_PASTELS\n$(cat src/cmd/acme/config.h)" > src/cmd/acme/config.h
-      echo -e "#define C_MIX DWhite\n$(cat src/cmd/acme/config.h)" > src/cmd/acme/config.h
+      cat > src/cmd/acme/config.h <<EOF
+/*
+ *  fontnames-array takes two fonts, first one
+ *  it treats as a proportional-width font and uses
+ *  everywhere possible and as a main UI font, while
+ *  the second font it treats as a fixed-width font,
+ *  changeable to any text window by executing "Font"
+ *  from tag window.
+ *
+ *  Note: "Font" can also be executed
+ *  with arguments with any "fontsrv -p ." approved
+ *  fonts, thus allowing you to experiment with font
+ *  sizes and whether you want anti-aliasing or not.
+ */
 
-      substituteInPlace src/cmd/acme/config.h \
-        --replace "#define C_TAGBG			0xFFFFFFFF" "#define C_TAGBG	DPalebluegreen" \
-        --replace "#define C_TAGFG			0x000000FF" "#define C_TAGFG	0x000000FF" \
-        --replace "#define C_TAGHLBG		0x999999FF" "#define C_TAGHLBG	DPalegreygreen" \
-        --replace "#define C_TAGHLFG		0x000000FF" "#define C_TAGHLFG	0x000000FF" \
-        --replace "#define C_TXTBG			0xFFFFFFFF" "#define C_TXTBG	DPaleyellow" \
-        --replace "#define C_TXTFG			0x000000FF" "#define C_TXTFG	0x000000FF" \
-        --replace "#define C_TXTHLBG		0x999999FF" "#define C_TXTHLBG	DDarkyellow" \
-        --replace "#define C_TXTHLFG		0x000000FF" "#define C_TXTHLFG	0x000000FF" \
-        --replace "#define C_WINBUTTON		0x4d4d4dFF" "#define C_WINBUTTON	DPurpleblue" \
-        --replace "#define C_COLBUTTON		0x55aaaaFF" "#define C_COLBUTTON	DPurpleblue" \
-        --replace "#define C_TMPBUTTON		0x55aaaaFF" "#define C_TMPBUTTON	DMedblue" \
-        --replace "#define C_SCROLLBG		0x999999FF" "#define C_SCROLLBG		DYellowgreen" \
-        --replace "#define C_BUTTON2HL		0x55aaaaFF" "#define C_BUTTON2HL	0xAA0000FF" \
-        --replace "#define C_BUTTON3HL		0x55aaaaFF" "#define C_BUTTON3HL	0x006600FF"
+char *fontnames[2] = {
+	"/mnt/font/RobotoMono-Regular/12a/font",
+	"/mnt/font/RobotoMono-Regular/12a/font"
+};
+
+/*
+ *  globalautoindent tries to guess where to
+ *  indent by the context of the previous line.
+ *
+ *  comes highly suggested.
+ */
+
+int globalautoindent	= TRUE;
+
+/*
+ *  swapping scroll buttons makes B1 scroll down
+ *  instead of up and B2 up instead of down
+ */
+
+int	swapscrollbuttons	= FALSE;
+
+/*
+ *  bartflag triggers quote unquote experimental
+ *  mode, in which instead of mouse following focus
+ *  from window to window etc. it in fact, does not.
+ *
+ *  comes highly suggested.
+ */
+
+int bartflag			= FALSE;
+
+/*
+ *  colors-constants. now let me take a minute to explain
+ *  them. C_TAGBG/FG/HLBG/HLFG are respectively your tag
+ *  windows background, foreground, highlighted back- and
+ *  foreground color. The same exact applies to C_TXT*-
+ *  variants of the same constant just in this case it handles
+ *  the colorscheme of the text window.
+ *
+ *  C_WINBUTTON is the button which you adjust your columns
+ *  position with. C_COLBUTTON represents a separate text
+ *  windows button. C_TMPBUTTON is the so-called "dirty"
+ *  or "unclean" -marker aka notifying the file as modified put
+ *  in the middle of C_COLBUTTON. C_SCROLLBG is plain and simple
+ *  your scrollbars background color.
+ *
+ *  the last two defines are for defining the color of the specific
+ *  buttons highlight background.
+ */
+
+#define ENABLE_PASTELS
+#define C_MIX DWhite
+
+#define C_TAGBG			DPalebluegreen
+#define C_TAGFG			0x000000FF
+#define C_TAGHLBG		DPalegreygreen
+#define C_TAGHLFG		0x000000FF
+
+#define C_TXTBG			DPaleyellow
+#define C_TXTFG			0x000000FF
+#define C_TXTHLBG		DDarkyellow
+#define C_TXTHLFG		0x000000FF
+
+#define C_WINBUTTON   DPurpleblue
+#define C_COLBUTTON		DPurpleblue
+#define C_TMPBUTTON		DMedblue
+#define C_SCROLLBG		DYellowgreen
+
+#define C_BUTTON2HL		0xAA0000FF
+#define C_BUTTON3HL		0x006600FF
+EOF
 
       # deprecated flags
       find . -type f \
         -exec sed -i -e 's/_SVID_SOURCE/_DEFAULT_SOURCE/g' {} \; \
         -exec sed -i -e 's/_BSD_SOURCE/_DEFAULT_SOURCE/g' {} \;
+
       substituteInPlace bin/9c \
         --replace 'which uniq' '${pkgs.which}/bin/which uniq'
     '';
